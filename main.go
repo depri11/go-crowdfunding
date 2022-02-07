0522f4563f78f16b32164c84b2b73604ae216e65
depri11 <wirlandikadevri@gmail.com>
2022-02-07 12:04:49 +0700

    jwt auth, validate token

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+
+	"github.com/depri11/go-crowdfunding/auth"
+	"github.com/depri11/go-crowdfunding/handler"
+	"github.com/depri11/go-crowdfunding/user"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/go_crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	authService := auth.NewService()
+
+	userHandler := handler.NewUserHandler(userService, authService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+	api.POST("/sessions", userHandler.Login)
+	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
+	api.POST("/avatars", userHandler.UploadAvatar)
+
+	router.Run(":1000")
+}
